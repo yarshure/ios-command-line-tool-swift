b39ca517358105f5fa94f8d6bf664c8d8a5d5611
lk <lk@pangu.cc>
2019-11-28 17:21:52 +0800

    first commit

--- testswift/main.swift
+++ testswift/main.swift
@@ -5,7 +5,12 @@
 //  Created by yarshure on 11/28/19.
 //  Copyright © 2019 pg. All rights reserved.
 //
-
+import Foundation
 class testswift {
 
 }
+if CommandLine.arguments.count < 3 {
+    print("Usage: \(CommandLine.arguments[0]) <port> <config-file>")
+    exit(1)
+}
+
